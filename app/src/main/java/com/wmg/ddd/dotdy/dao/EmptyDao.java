17bf6799b39f61d7c142423f4dd07a8900ba2a9e
wu-mingguang <1540426810@qq.com>
2017-09-13 18:11:43 +0800

    fill some empty files

---
+++ app/src/main/java/com/wmg/ddd/dotdy/dao/EmptyDao.java
@@ -0,0 +1,8 @@
+package com.wmg.ddd.dotdy.dao;
+
+/**
+ * Created by wu-mingguang on 2017/9/13.
+ */
+
+public class EmptyDao {
+}
---
+++ app/src/main/java/com/wmg/ddd/dotdy/view/FunctionButton.java
@@ -0,0 +1,23 @@
+package com.wmg.ddd.dotdy.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by wu-mingguang on 2017/9/13.
+ */
+
+public class FunctionButton extends Button {
+    public FunctionButton(Context context) {
+        super(context);
+    }
+
+    public FunctionButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FunctionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
